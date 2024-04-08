ff75d6eeedf9df6f25a6ba34679fe59d7246df9f
Samuel <samuel_monroy1@baylor.edu>
2024-04-07 22:56:02 -0500

    Finished

--- list_comprehension_start_file.py
+++ list_comprehension_start_file.py
@@ -107,7 +107,9 @@ print(a)
 
 
 
-
+print()
+print("---Exercises---")
+print()
 
 ## Exercise ##
 
@@ -116,8 +118,8 @@ print(a)
 
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 
-
-
+new_numbers = [i for i in numbers if i >= 0]
+print(new_numbers)
 
 ## 2 create a list of integers which specify the length of each word in
 ## a sentence except for the word 'the'
@@ -125,7 +127,8 @@ numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 sentence = "the quick brown fox jumps over the lazy dog"
 words = sentence.split()
 
-
+lengths = [len(word) for word in words if word != 'the']
+print(lengths)
 
 ## Given dictionary is consisted of vehicles and their weights in kilograms. 
 ## Contruct a list of the names of vehicles with weight below 5000 kilograms. 
@@ -134,24 +137,24 @@ words = sentence.split()
 dict={"Sedan": 1500, "SUV": 2000, "Pickup": 2500, "Minivan": 1600, "Van": 2400, 
 "Semi": 13600, "Bicycle": 7, "Motorcycle": 110}
 
+vehicles = [vehicle.upper() for vehicle, weight in dict.items() if weight < 5000]
 
-
+print(vehicles)
 
 ## Find all the numbers from 1 to 1000 that have a 4 in them
-
-
+numbers = [num for num in range(1,1001) if "4" in str(num)]
+print(numbers)
 
 ## count how many times the word 'the' appears in the text file - 'sometext.txt'
-
-
+infile = open("sometext.txt", "r")
+content = infile.read()
+appearences = [1 for word in content.split() if word.lower() == 'the']
+print(f'"The" appears {len(appearences)} times')
 
 ## Extract the numbers from the following phrase ##
 
 phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each " \
 "event, with about 3 or 4 that were classifled as serious per event.'
 
-
-
-
-
-
+extraction = [x for x in phrase.split(" ") if x.isdigit()]
+print(extraction)
